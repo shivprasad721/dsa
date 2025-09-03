31096e11b452eb8d60b06f38952f4523067175d3
shivprasad721 <shivprasad721@gmail.com>
2025-09-04 00:45:03 +0530

    reverse string

--- Warm Up/reverseString.js
+++ Warm Up/reverseString.js
@@ -2,9 +2,9 @@ let arr = ["h","e","l","l","o"];
 var reverseString = function(arr) {
     let len = arr.length-1;
     for(let i = 0; i < Math.floor(arr.length/2); i++){
-        let temp = arr[i];
-        arr[i] = arr[len];
-        arr[len] = temp;
+        let temp = arr[len];
+        arr[len] = arr[i];
+        arr[i] = temp;
         len--;
     }
 }
